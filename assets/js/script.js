72f029f7ff7746f0f42cd822e4103d7aa7e75f4d
Jelena Jovicic <jejovicic@gmail.com>
2023-01-03 19:52:55 +0100

    Add read status on creation

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,27 +11,37 @@ function addBookToLibrary(book) {
   myLibrary.unshift(book);
 }
 
-/* Book.prototype.isRead = function isRead(checked) { // called when clicking on a checkbox, if it's checked the book is read
-  this.read = checked ?? false; // nuelish coaelescing, because it's also called on load before any checking, so return false if undefined
-}; */
+Book.prototype.isRead = function isRead(checked) {
+  if (checked) {
+    this.read = "Yes.";
+  } else {
+    this.read = "Not read yet.";
+  }
+};
 
 const template = document.getElementById("card-template");
 
+const inputStatus = document.getElementById("status");
+
 Book.prototype.createCard = function createCard() {
   const displayItem = template.content.cloneNode(true);
-  const displayTitle = displayItem.querySelector(".book-title");
+  const displayTitle = displayItem.querySelector(".card-title");
   displayTitle.textContent = this.title;
-  const displayAuthor = displayItem.querySelector(".book-author");
-  displayAuthor.textContent = `by ${this.author};`;;
-  const displayPages = displayItem.querySelector(".book-pages");
+  const displayAuthor = displayItem.querySelector(".card-author");
+  displayAuthor.textContent = `by ${this.author};`;
+  const displayPages = displayItem.querySelector(".card-pages");
   displayPages.textContent = `${this.pages} pages.`;
-  const displayStatus = displayItem.querySelector(".book-read");
-  displayStatus.textContent = `Not read yet.`;
-  const deleteItem = displayItem.querySelector(".book-delete");
+  const displayStatus = displayItem.querySelector(".card-read");
+  this.isRead(inputStatus.checked);
+  /* const editStatus = displayItem.querySelector(".edit-status");
+  editStatus.addEventListener("change", () => {
+  }) */
+  displayStatus.textContent = this.read;
+  const deleteItem = displayItem.querySelector(".card-delete");
   displayLibrary.insertBefore(displayItem, displayLibrary.firstChild);
   deleteItem.addEventListener("click", (e) => {
     myLibrary.splice(myLibrary.indexOf(this), 1);
-    e.currentTarget.parentNode.remove()
+    e.currentTarget.parentNode.remove();
   });
 };
 
@@ -46,24 +56,36 @@ const inputTitle = document.getElementById("title");
 const inputAuthor = document.getElementById("author");
 const inputPages = document.getElementById("pages");
 
+/* inputStatus.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    console.log(inputStatus.value)
+    console.log(inputStatus.checked)
+    return true
+  } 
+    console.log(inputStatus.value)
+    console.log(inputStatus.checked)
+    return false
+  
+  
+}); */
+
 function getValue(e) {
   const tempBook = new Book(
     inputTitle.value,
     inputAuthor.value,
-    Number(inputPages.value),
+    Number(inputPages.value)
   );
+  // console.log(inputStatus.checked)
+  // tempBook.isRead(inputStatus.checked);
   addBookToLibrary(tempBook);
   tempBook.createCard();
   form.style.display = "none";
   btnShowForm.style.display = "block";
-  inputTitle.value = "";
-  inputAuthor.value = "";
-  inputPages.value = "";
+  form.reset();
   e.preventDefault();
 }
 
-const btnAddBook = document.getElementById("add-book");
-btnAddBook.addEventListener("click", getValue);
+form.addEventListener("submit", getValue);
 
 // temporary manually added books, free to delete later:
 
@@ -74,18 +96,22 @@ const vragolaniIDzangrizala = new Book(
   "Toni Vulf",
   44
 );
-const glineniUdar = new Book("Glineni udar", "Donald Lemke", 41); 
+const glineniUdar = new Book("Glineni udar", "Donald Lemke", 41);
 
 addBookToLibrary(kockar);
 addBookToLibrary(priceIzSume);
 addBookToLibrary(vragolaniIDzangrizala);
-addBookToLibrary(glineniUdar); 
+addBookToLibrary(glineniUdar);
+
+kockar.isRead();
+priceIzSume.isRead();
+vragolaniIDzangrizala.isRead();
+glineniUdar.isRead();
 
 kockar.createCard();
 priceIzSume.createCard();
 vragolaniIDzangrizala.createCard();
 glineniUdar.createCard();
+console.table(myLibrary);
 
 // end temporary
-
-
