f0548f3e2dc879f919059749969327cec23d4be1
Jelena Jovicic <jejovicic@gmail.com>
2023-01-04 12:17:32 +0100

    Add unique fors and ids for edit read checkbox, now it works when clicking on label

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 const myLibrary = [];
 const displayLibrary = document.querySelector("#library");
 
+const form = document.querySelector("form");
+const inputTitle = document.getElementById("input-title");
+const inputAuthor = document.getElementById("input-author");
+const inputPages = document.getElementById("input-pages");
+const inputRead = document.getElementById("input-read");
+
+const template = document.getElementById("card-template");
+
 function Book(title, author, pages) {
   this.title = title;
   this.author = author;
@@ -19,99 +27,98 @@ Book.prototype.isRead = function isRead(checked) {
   }
 };
 
-const template = document.getElementById("card-template");
-
-const inputStatus = document.getElementById("status");
-
 Book.prototype.createCard = function createCard() {
-  const displayItem = template.content.cloneNode(true);
-  const displayTitle = displayItem.querySelector(".card-title");
-  displayTitle.textContent = this.title;
-  const displayAuthor = displayItem.querySelector(".card-author");
-  displayAuthor.textContent = `by ${this.author};`;
-  const displayPages = displayItem.querySelector(".card-pages");
-  displayPages.textContent = `${this.pages} pages.`;
-  const displayStatus = displayItem.querySelector(".card-read");
-  this.isRead(inputStatus.checked);
-  /* const editStatus = displayItem.querySelector(".edit-status");
-  editStatus.addEventListener("change", () => {
-  }) */
-  displayStatus.textContent = this.read;
-  const deleteItem = displayItem.querySelector(".card-delete");
-  displayLibrary.insertBefore(displayItem, displayLibrary.firstChild);
-  deleteItem.addEventListener("click", (e) => {
+  const card = template.content.cloneNode(true);
+  const cardTitle = card.querySelector(".card-title");
+  cardTitle.textContent = this.title;
+  const cardAuthor = card.querySelector(".card-author");
+  cardAuthor.textContent = `by ${this.author};`;
+  const cardPages = card.querySelector(".card-pages");
+  cardPages.textContent = `${this.pages} pages.`;
+  const cardEditRead = card.querySelector(".edit-read");
+  const editReadLabel = card.querySelector(".edit-read-label")
+  editReadLabel.setAttribute("for", `edit-read-${Date.now() + myLibrary.indexOf(this)}`)
+  cardEditRead.setAttribute("id", editReadLabel.getAttribute("for")) // checkbox and label must have matching and unique ids and fors
+  cardEditRead.checked = inputRead.checked; // if checked on creation leave it checked
+  const cardRead = card.querySelector(".card-read");
+  this.isRead(inputRead.checked);
+  cardRead.textContent = this.read;
+  const cardDelete = card.querySelector(".card-delete");
+  displayLibrary.insertBefore(card, displayLibrary.firstChild);
+
+  cardEditRead.addEventListener("click", () => {
+    this.isRead(cardEditRead.checked);
+    cardRead.textContent = this.read;
+  }); 
+  cardDelete.addEventListener("click", (e) => {
     myLibrary.splice(myLibrary.indexOf(this), 1);
-    e.currentTarget.parentNode.remove();
+    e.currentTarget.parentNode.remove(); // same as card.remove() but it won't work because card is not added to DOM at this point.
   });
 };
 
-const form = document.querySelector("form");
-const btnShowForm = document.getElementById("show-form");
-btnShowForm.addEventListener("click", () => {
+
+const btnCreate = document.getElementById("btn-create");
+btnCreate.addEventListener("click", () => {
   form.style.display = "block";
-  btnShowForm.style.display = "none";
+  btnCreate.style.display = "none";
 });
 
-const inputTitle = document.getElementById("title");
-const inputAuthor = document.getElementById("author");
-const inputPages = document.getElementById("pages");
-
-/* inputStatus.addEventListener("change", (e) => {
-  if (e.target.checked) {
-    console.log(inputStatus.value)
-    console.log(inputStatus.checked)
-    return true
-  } 
-    console.log(inputStatus.value)
-    console.log(inputStatus.checked)
-    return false
-  
-  
-}); */
-
-function getValue(e) {
+function createBook(e) {
   const tempBook = new Book(
     inputTitle.value,
     inputAuthor.value,
     Number(inputPages.value)
   );
-  // console.log(inputStatus.checked)
-  // tempBook.isRead(inputStatus.checked);
   addBookToLibrary(tempBook);
   tempBook.createCard();
   form.style.display = "none";
-  btnShowForm.style.display = "block";
+  btnCreate.style.display = "block";
   form.reset();
   e.preventDefault();
 }
 
-form.addEventListener("submit", getValue);
+form.addEventListener("submit", createBook);
 
 // temporary manually added books, free to delete later:
 
-const kockar = new Book("Kockar", "F.M.Dostojevski", 186);
-const priceIzSume = new Book("Priče iz šume", "Nikoleta Novak", 146, false);
+const kockar = new Book("Kockar", "F. M. Dostojevski", 186);
+const priceIzSume = new Book("Priče iz šume", "Nikoleta Novak", 146);
 const vragolaniIDzangrizala = new Book(
   "Vragolani i džangrizala",
   "Toni Vulf",
   44
 );
 const glineniUdar = new Book("Glineni udar", "Donald Lemke", 41);
+const idiot = new Book("Idiot", "F. M. Dostojevski", 681);
+const desetLjutihGusara = new Book("Deset Ljutih Gusara", "Ljubivoje Ršumović", 30);
+const najgoriUciteljiNaSvetu = new Book("Najgori učitelji na svetu", "Dejvid Valijams", 304);
+const priceSaImanja = new Book("Priče sa imanja", "Paskal Veder d'Orija, Pjer Kuron", 112)
 
 addBookToLibrary(kockar);
 addBookToLibrary(priceIzSume);
 addBookToLibrary(vragolaniIDzangrizala);
 addBookToLibrary(glineniUdar);
+addBookToLibrary(idiot);
+addBookToLibrary(desetLjutihGusara);
+addBookToLibrary(najgoriUciteljiNaSvetu);
+addBookToLibrary(priceSaImanja);
 
 kockar.isRead();
 priceIzSume.isRead();
 vragolaniIDzangrizala.isRead();
 glineniUdar.isRead();
+idiot.isRead();
+desetLjutihGusara.isRead();
+najgoriUciteljiNaSvetu.isRead();
+priceSaImanja.isRead();
 
 kockar.createCard();
 priceIzSume.createCard();
 vragolaniIDzangrizala.createCard();
 glineniUdar.createCard();
-console.table(myLibrary);
+idiot.createCard();
+desetLjutihGusara.createCard();
+najgoriUciteljiNaSvetu.createCard();
+priceSaImanja.createCard();
 
 // end temporary
