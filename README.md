015da048bcdcf859a83bcf008c4d97928a8b8b57
Jelena Jovicic <jejovicic@gmail.com>
2023-01-09 12:54:16 +0100

    Improve checkboxes logic

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ A Library app to keep track of books. Built using JavaScript, HTML, and Sass for
 
 ### Description
 
-...
+A small book tracking app made as a part of [The Odin Project](https://www.theodinproject.com) curriculum, with added functionality to keep track of dates when books are due to be returned to the library.
 
 ### Screenshot
 
@@ -43,12 +43,14 @@ A Library app to keep track of books. Built using JavaScript, HTML, and Sass for
 
 ### The process
 
-...
+This project uses templating technique to clone a "card" element instead of creating a new one each time a new book is created. 
 
 ### Useful resources
 
 - [How To Lint and Format Code with ESLint in Visual Studio Code](https://www.digitalocean.com/community/tutorials/linting-and-formatting-with-eslint-in-vs-code) - A tutorial for setting up ESLint for a project.
 - [Templating in HTML](https://kittygiraudel.com/2022/09/30/templating-in-html/) - "Long story short, templates are good to avoid creating complex DOM structures by hand."
+- [7-1 pattern boilerplate](https://github.com/KittyGiraudel/sass-boilerplate/tree/master/stylesheets) - A boilerplate for 7-1 Sass Architecture.
+- [HTML / Sass Jumpstart]() - Mostly plucked some sensible values (font sizes, spacings, etc) from here.
 
 ## Author
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
 const myLibrary = [];
-const displayLibrary = document.querySelector("#library");
+const displayLibrary = document.getElementById("library");
 
-const form = document.querySelector("form");
+const form = document.getElementById("form-add");
 const inputTitle = document.getElementById("input-title");
 const inputAuthor = document.getElementById("input-author");
 const inputPages = document.getElementById("input-pages");
-const inputRead = document.getElementById("input-read");
 
 const template = document.getElementById("card-template");
 
@@ -21,10 +20,18 @@ function addBookToLibrary(book) {
 
 Book.prototype.setId = function setId() {
   this.id = Date.now() + myLibrary.indexOf(this);
-}
+};
 
-Book.prototype.isRead = function isRead(checked) {
-  checked ? this.read = "Yes." : this.read = "Not read yet."
+Book.prototype.toggleStatus = function toggleStatus(key, checked) {
+  this[key] = !!checked; // if checkbox checked, set value of object's key to true
+};
+
+Book.prototype.updateLabelText = function updateLabelText(prop) {
+  const string = String(prop);
+  if (this[prop]) {
+    return `${string.at(0).toUpperCase() + string.slice(1)}.`;
+  }
+  return `Not ${string} yet.`;
 };
 
 Book.prototype.createCard = function createCard() {
@@ -36,21 +43,35 @@ Book.prototype.createCard = function createCard() {
   cardAuthor.textContent = `by ${this.author};`;
   const cardPages = card.querySelector(".card-pages");
   cardPages.textContent = `${this.pages} pages.`;
-  const cardEditRead = card.querySelector(".edit-read");
-  const editReadLabel = card.querySelector(".edit-read-label")
-  editReadLabel.setAttribute("for", `edit-read-${this.id}`)
-  cardEditRead.setAttribute("id", editReadLabel.getAttribute("for")) // checkbox and label must have matching and unique ids and fors
-  cardEditRead.checked = inputRead.checked; // if checked on creation leave it checked
-  const cardRead = card.querySelector(".card-read");
-  this.isRead(inputRead.checked);
-  cardRead.textContent = this.read;
-  const cardDelete = card.querySelector(".card-delete");
-  displayLibrary.insertBefore(card, displayLibrary.firstChild);
 
-  cardEditRead.addEventListener("click", () => {
-    this.isRead(cardEditRead.checked);
-    cardRead.textContent = this.read;
-  }); 
+  const cardReadCheckbox = card.querySelector(".card-read-checkbox");
+  const cardReadLabel = card.querySelector(".card-read-label");
+  const cardReturnedCheckbox = card.querySelector(".card-returned-checkbox");
+  const cardReturnedLabel = card.querySelector(".card-returned-label");
+  cardReadLabel.setAttribute("for", `card-read-${this.id}`);
+  cardReadCheckbox.setAttribute("id", cardReadLabel.getAttribute("for")); // checkbox and label must have matching and unique ids and fors
+  cardReturnedLabel.setAttribute("for", `card-returned-${this.id}`);
+  cardReturnedCheckbox.setAttribute(
+    "id",
+    cardReturnedLabel.getAttribute("for")
+  );
+
+  const cardDelete = card.querySelector(".card-delete");
+  displayLibrary.insertBefore(card, displayLibrary.firstChild); // add new books to top
+
+  cardReadCheckbox.addEventListener("click", () => {
+    this.toggleStatus("read", cardReadCheckbox.checked);
+    cardReadLabel.childNodes[1].textContent = this.updateLabelText("read");
+    // textContent on label alone won't work because checkbox is nested within the label so changing text content would overwrite the checkbox itself.
+  });
+
+  cardReturnedCheckbox.addEventListener("click", () => {
+    this.toggleStatus("returned", cardReturnedCheckbox.checked);
+    this.updateLabelText(cardReturnedLabel, "returned");
+    cardReturnedLabel.childNodes[1].textContent =
+      this.updateLabelText("returned");
+  });
+
   cardDelete.addEventListener("click", (e) => {
     if (window.confirm(`Are you sure you want to delete "${this.title}"`)) {
       myLibrary.splice(myLibrary.indexOf(this), 1);
@@ -58,7 +79,6 @@ Book.prototype.createCard = function createCard() {
     }
   });
 };
-
 
 const btnCreate = document.getElementById("btn-create");
 btnCreate.addEventListener("click", () => {
@@ -93,9 +113,21 @@ const vragolaniIDzangrizala = new Book(
 );
 const glineniUdar = new Book("Glineni udar", "Donald Lemke", 41);
 const idiot = new Book("Idiot", "F. M. Dostojevski", 681);
-const desetLjutihGusara = new Book("Deset Ljutih Gusara", "Ljubivoje Ršumović", 30);
-const najgoriUciteljiNaSvetu = new Book("Najgori učitelji na svetu", "Dejvid Valijams", 304);
-const priceSaImanja = new Book("Priče sa imanja", "Paskal Veder d'Orija, Pjer Kuron", 112)
+const desetLjutihGusara = new Book(
+  "Deset Ljutih Gusara",
+  "Ljubivoje Ršumović",
+  30
+);
+const najgoriUciteljiNaSvetu = new Book(
+  "Najgori učitelji na svetu",
+  "Dejvid Valijams",
+  304
+);
+const priceSaImanja = new Book(
+  "Priče sa imanja",
+  "Paskal Veder d'Orija, Pjer Kuron",
+  112
+);
 
 addBookToLibrary(kockar);
 addBookToLibrary(priceIzSume);
@@ -106,14 +138,22 @@ addBookToLibrary(desetLjutihGusara);
 addBookToLibrary(najgoriUciteljiNaSvetu);
 addBookToLibrary(priceSaImanja);
 
-kockar.isRead();
-priceIzSume.isRead();
-vragolaniIDzangrizala.isRead();
-glineniUdar.isRead();
-idiot.isRead();
-desetLjutihGusara.isRead();
-najgoriUciteljiNaSvetu.isRead();
-priceSaImanja.isRead();
+kockar.toggleStatus("read");
+priceIzSume.toggleStatus("read");
+vragolaniIDzangrizala.toggleStatus("read");
+glineniUdar.toggleStatus("read");
+idiot.toggleStatus("read");
+desetLjutihGusara.toggleStatus("read");
+najgoriUciteljiNaSvetu.toggleStatus("read");
+priceSaImanja.toggleStatus("read");
+kockar.toggleStatus("returned");
+priceIzSume.toggleStatus("returned");
+vragolaniIDzangrizala.toggleStatus("returned");
+glineniUdar.toggleStatus("returned");
+idiot.toggleStatus("returned");
+desetLjutihGusara.toggleStatus("returned");
+najgoriUciteljiNaSvetu.toggleStatus("returned");
+priceSaImanja.toggleStatus("returned");
 
 kockar.createCard();
 priceIzSume.createCard();
@@ -126,9 +166,10 @@ priceSaImanja.createCard();
 
 // end temporary
 
-//example book
+// example book
 
 /* const desetLjutihGusara = new Book("Deset Ljutih Gusara", "Ljubivoje Ršumović", 30);
 addBookToLibrary(desetLjutihGusara);
-desetLjutihGusara.isRead();
+desetLjutihGusara.toggleStatus("read");
+desetLjutihGusara.toggleStatus("returned");
 desetLjutihGusara.createCard(); */
